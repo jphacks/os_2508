e3978b4a205ad5968172945807ec75ed07ba7e3d
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-10-18 11:31:37 +0900

    CookieObserver.js: Done
    Path: Projects/Backend/Workspace/Server.js,
    Projects/Backend/Workspace/Tools/CookieObserver.js

--- Projects/Backend/Workspace/Server.js
+++ Projects/Backend/Workspace/Server.js
@@ -1,13 +1,29 @@
+// 最初にすべきこと
 const express = require('express');
 const path = require('path');
-
+const dotenv = require('dotenv').config();
+const cookieParser = require("cookie-parser");
 const app = express();
-const PORT = 3000;
 
-app.use(express.json());
+// use系を使う
+app.use(express.json()); // json解析してreq.bodyに自動的に格納する。(Formで送られた物以外)
+app.use(cookieParser()); // cookie解析してreq.cookiesに自動的に格納する。
+app.use(express.urlencoded({ extended: true })); // POSTされたformの内容を解析してreq.bodyに自動的に格納する。
 
-app.get("/", (req, res) => {
-    res.send("test");
+// ページの配信(Reactでbuild予定)
+app.use(express.static(path.join(__dirname, "..", "..", "..", "Frontend", "dist")));
+
+// ルーティング(申請してくれたら追加します)
+const authRoutes = require('./Routes/AuthRoutes');
+app.use("/Auth", authRoutes);
+
+// HomeRouting
+app.get("/Home", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "..", "..", "Frontend", "dist", "index.html"));
 });
 
+// PORT番号を.envから取得
+const PORT = process.env.PORT || 5000;
+
+// Serverを起動
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running at http://localhost:${PORT}`))
