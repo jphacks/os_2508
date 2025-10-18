24c6520e85e82348249f4172b443db0eb9f31896
Child-Of-God-Of-God <child.of.god.of.god.child@gmail.com>
2025-10-18 11:32:59 +0900

    Auth: Done
    Projects/Backend/Workspace/Routes/AuthRoutes.js

--- Projects/Backend/Workspace/Routes/AuthRoutes.js
+++ Projects/Backend/Workspace/Routes/AuthRoutes.js
@@ -1,17 +1,20 @@
-const invitedKey = req.body.invitedKey
-const INVITED_KEY = process.env.INVITED_KEY
-const dotenv = require("dotenv");
+const invitedKey = req.body.invitedKey    // ユーザーが入力した招待コード
+const INVITED_KEY = process.env.INVITED_KEY    // 招待コード
 
+// 違う階層から.envを読み込む処理
+const dotenv = require("dotenv");
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 if (invitedKey == INVITED_KEY) {
     const status = { status: "success" };
-    const AUTH_SECRET = process.env.AUTH_SECRET
-    const AUTH_REFRESH_SECRET = process.env.AUTH_REFRESH_SECRET
 
-    const token = jwt.sign(status, AUTH_SECRET, { expiresIn: '1h' });
-    const refreshToken = jwt.sign(status, AUTH_REFRESH_SECRET, { expiresIn: '7d' });
+    const AUTH_SECRET = process.env.AUTH_SECRET    //.env内のトークンの秘密鍵
+    const AUTH_REFRESH_SECRET = process.env.AUTH_REFRESH_SECRET    //.env内のリフレッシュトークンの秘密鍵
 
+    const token = jwt.sign(status, AUTH_SECRET, { expiresIn: '1h' });    // トークンの準備
+    const refreshToken = jwt.sign(status, AUTH_REFRESH_SECRET, { expiresIn: '7d' });    // リフレッシュトークンの準備
+
+    //cookie返却
     res.cookie('InvitedToken', token, { httpOnly: false, sameSite: 'strict', expiresIn: '1h' });
     res.cookie('InvitedRefreshToken', refreshToken, { httpOnly: true, expiresIn: '90d' });
 
