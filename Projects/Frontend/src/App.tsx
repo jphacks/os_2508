fa3e257071aa0fe6e07a2e9ce8f4ddda47c624a6
nayu <is0762ix@ed.ritsumei.ac.jp>
2025-10-18 15:57:37 +0900

    Done
    Path
    /GitHub/ARROW-demo/Projects/Frontend/react-files/src/Workspace/pages/EventDetai
    
    Description
    詳細ページ完成

--- Projects/Frontend/src/App.tsx
+++ Projects/Frontend/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from "./Workspace/pages/Home/Home";
-// import Auth from "./Workspace/pages/Auth/Auth";
+//import Auth from "./Workspace/Pages/Auth/Auth";
 // import Register from "./Workspace/pages/Register/Register"; 
 import Profile from "./Workspace/Pages/Profile/Profile"; 
-// import Application from "./Workspace/pages/Application/Application";
+import EventDetail from "./Workspace/Pages/EventDetail/EventDetail";
 // import Operation from "./Workspace/pages/Operation/Operation";
 // import Edit from "./Workspace/pages/Edit/Edit";
-// import Event from "./Workspace/pages/Event/Event"; 
+//import Event from "./Workspace/pages/Event/Event"; 
 // import Login from "./Workspace/pages/Login/Login"; 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Profile />}/>
+        <Route path="/" element={<EventDetail />}/>
         <Route path="/Profile" element={<Profile />}/>
         {/* <Route path="/Home" element={<Home />}/>
         <Route path="/Login" element={<Login />}/>
