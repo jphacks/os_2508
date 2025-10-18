c7c9e3894d770b6cd3c02713bb6860e5c4aec923
nayu <is0762ix@ed.ritsumei.ac.jp>
2025-10-18 17:38:49 +0900

    Done
    Path
    /GitHub/ARROW-demo/Projects/Frontend/react-files/src/Workspace/pages/EventEdit
    
    Description
    完成

--- Projects/Frontend/src/App.tsx
+++ Projects/Frontend/src/App.tsx
@@ -5,21 +5,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./Workspace/Pages/Profile/Profile"; 
 import EventDetail from "./Workspace/Pages/EventDetail/EventDetail";
 // import Operation from "./Workspace/pages/Operation/Operation";
-// import Edit from "./Workspace/pages/Edit/Edit";
-//import Event from "./Workspace/pages/Event/Event"; 
+import EventEdit from "./Workspace/Pages/EventEdit/EventEdit";
+import Event from "./Workspace/Pages/Event/Event"; 
 // import Login from "./Workspace/pages/Login/Login"; 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<EventDetail />}/>
+        <Route path="/" element={<EventEdit />}/>
         <Route path="/Profile" element={<Profile />}/>
+        <Route path="/Event" element={<Event />}/>
+        <Route path="/Home/Even/EventDetail" element={<EventDetail />}/>
+        <Route path="/Home/Event/Operation/EventEdit" element={<EventEdit />}/>
         {/* <Route path="/Home" element={<Home />}/>
         <Route path="/Login" element={<Login />}/>
         <Route path="/Register" element={<Register />}/>
-        <Route path="/Event" element={<Event />}/>
-        <Route path="/Home/Even/Application" element={<Application />}/>
-        <Route path="/Home/Event/Operation/Edit" element={<Edit />}/>
         <Route path="/Home/Event/Operation/Operation" element={<Operation />}/> */}
       </Routes>
     </BrowserRouter>
